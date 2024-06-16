8a48d3830566b8d3018c9b523ec7f36c5a15dac9
utopiaexpress123 <ricksanchezy2x@gmail.com>
2024-06-16 19:07:19 +0200

    llll

--- app/call/page.tsx
+++ app/call/page.tsx
@@ -1,12 +1,40 @@
 import { getHumeAccessToken } from "@/utils/getHumeAccessToken";
 import dynamic from "next/dynamic";
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 
 const Chat = dynamic(() => import("@/components/Chat"), {
   ssr: false,
 });
 
+
+
 export default async function Page() {
+  const supabase = createClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  const { data: userDetails } = await supabase
+    .from('users')
+    .select('*')
+    .single();
+
+  const { data: subscription, error } = await supabase
+    .from('subscriptions')
+    .select('*, prices(*, products(*))')
+    .in('status', ['trialing', 'active'])
+    .maybeSingle();
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (!user) {
+    return redirect('/signin');
+  }
   const accessToken = await getHumeAccessToken();
 
   if (!accessToken) {
